e889c7de89c4049deaab6e18dee584b9ff2cef56
kohei <sky6sk212@gmail.com>
2020-09-10 17:59:48 +0900

    exercise CRUD

--- app/javascript/Top.vue
+++ app/javascript/Top.vue
@@ -12,7 +12,6 @@
           :title="e.name"
           :volume="e.amount"
           :nutrition="e.nutrition"
-          @click="foodDialogVisible = true"
         ></FoodCard>
       </template>
       <div class="card" @click="foodDialogVisible = true">
@@ -40,14 +39,15 @@
     <div class="flex">
       <template v-for="(e, key) in exercises">
         <ExerciseCard
+          @click.native="changeForm(e)"
           :key="key"
+          :id="e.id"
           :title="e.name"
           :time="e.time"
           :momentum="e.momentum"
-          @click="exerciseDialogVisible = true"
         ></ExerciseCard>
       </template>
-      <div class="card" @click="exerciseDialogVisible = true">
+      <div class="card" @click="newExercise">
         <p class="add">追加する</p>
         <i class="el-icon-circle-plus-outline"></i>
       </div>
@@ -56,7 +56,7 @@
     <el-dialog title="どんな運動した？" :visible.sync="exerciseDialogVisible">
       <el-form :model="exerciseForm">
         <el-form-item label="運動名" :label-width="formLabelWidth">
-          <el-select v-model="exerciseForm.nam" placeholder="選択してください">
+          <el-select v-model="exerciseForm.name" placeholder="選択してください">
             <el-option
               v-for="item in options"
               :key="item.value"
@@ -65,16 +65,22 @@
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="運動した時間" :label-width="formLabelWidth">
-          <el-input v-model="exerciseForm.time" autocomplete="off"></el-input>
+        <el-form-item label="運動時間(分)" :label-width="formLabelWidth">
+          <el-select v-model="exerciseForm.time" placeholder="選択してください">
+            <el-option v-for="i of 30" :key="i" :label="i*10" :value="i*10"></el-option>
+          </el-select>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="exerciseDialogVisible = false">追加する</el-button>
+        <el-button type="primary" @click="addExercise" v-if="exerciseForm.id === -1">追加する</el-button>
+        <div class="flex_button">
+          <el-button type="danger" @click="deleteExercise" v-if="exerciseForm.id > -1">削除する</el-button>
+          <el-button type="warning" @click="updateExercise" v-if="exerciseForm.id > -1">編集する</el-button>
+        </div>
       </span>
     </el-dialog>
 
-    <el-button type="warning">レシピの提案</el-button>
+    <el-button type="warning" @click="goRecipe">レシピの提案</el-button>
   </div>
 </template>
 
@@ -114,25 +120,27 @@ export default {
         volume: "",
       },
       exerciseForm: {
+        id: -1,
         name: "",
-        time: "",
+        time: 0,
+        momentum: 0,
       },
       options: [
         {
-          value: 'フットサル',
-          label: 'フットサル'
-        }, 
-        {
-          value: 'ランニング',
-          label: 'ランニング'
+          value: "フットサル",
+          label: "フットサル",
         },
         {
-          value: '腹筋',
-          label: '腹筋'
-        }, 
+          value: "ランニング",
+          label: "ランニング",
+        },
         {
-          value: '腕立て',
-          label: '腕立て'
+          value: "腹筋",
+          label: "腹筋",
+        },
+        {
+          value: "腕立て",
+          label: "腕立て",
         },
       ],
       formLabelWidth: "120px",
@@ -143,7 +151,97 @@ export default {
       this.exercises = response.data;
     });
   },
-  methods: {},
+  methods: {
+    exerciseFormInit() {
+      this.$set(this.exerciseForm, "id", -1);
+      this.$set(this.exerciseForm, "name", "");
+      this.$set(this.exerciseForm, "time", 0);
+      this.$set(this.exerciseForm, "momentum", 0);
+    },
+    updateCards() {
+      axios.get("/api/v1/exercises.json").then((response) => {
+        this.exercises = response.data;
+      });
+    },
+    checkForm() {
+      const exerciseName = this.exerciseForm.name;
+      if (exerciseName === "") {
+        alert("運動名を選択してください");
+        return;
+      }
+
+      if (this.exerciseForm.time === 0) {
+        alert("運動時間を選択してください");
+        return;
+      }
+
+      // 入力されていたら運動量の計算
+      let METs = 0;
+      if (exerciseName === "フットサル") {
+        METs = 9;
+      } else if (exerciseName === "ランニング") {
+        METs = 11;
+      } else if (exerciseName === "腹筋" || exerciseName === "腕立て") {
+        METs = 5;
+      }
+      const bodyWeight = 65;
+      this.exerciseForm.momentum = Math.round(
+        METs * bodyWeight * (this.exerciseForm.time / 60)
+      );
+
+      this.exerciseDialogVisible = false;
+    },
+    changeForm(e) {
+      this.exerciseDialogVisible = true;
+      this.$set(this.exerciseForm, "id", e.id);
+      this.$set(this.exerciseForm, "name", e.name);
+      this.$set(this.exerciseForm, "time", e.time);
+      this.$set(this.exerciseForm, "momentum", e.momentum);
+    },
+    updateExercise() {
+      this.checkForm();
+      axios
+        .patch(`/api/v1/exercises/${this.exerciseForm.id}`, this.exerciseForm)
+        .then((response) => {
+          this.exerciseFormInit();
+          this.updateCards();
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    deleteExercise() {
+      axios
+        .delete(`/api/v1/exercises/${this.exerciseForm.id}`)
+        .then((response) => {
+          this.exerciseFormInit();
+          this.updateCards();
+          this.exerciseDialogVisible = false;
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    newExercise() {
+      this.exerciseFormInit();
+      this.exerciseDialogVisible = true;
+    },
+    addExercise() {
+      this.checkForm();
+      axios
+        .post("/api/v1/exercises", this.exerciseForm)
+        .then((response) => {
+          this.exerciseFormInit();
+          this.updateCards();
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    goRecipe() {
+      this.$router.push({ path: "recipe" });
+    },
+  },
 };
 </script>
 
@@ -180,12 +278,22 @@ button {
   background-color: #fff;
   width: calc(33.33333% - 42px);
   margin: 20px;
-  height: 345px;
+  height: 304px;
 }
 .add {
   margin-top: 100px;
 }
 .el-icon-circle-plus-outline {
   font-size: 50px;
+}
+.el-input {
+  width: 160px;
+}
+#top >>> .el-dialog {
+  width: 400px !important;
+}
+.flex_button {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
